daf917d0648987fb7600ba73d74ffab7625c66ce
jmarquez <jmarquez@node0.jmarquez-qv74453.hpccloud-pg0.wisc.cloudlab.us>
2020-06-22 18:41:17 -0500

    adding repo and -y

--- install_beegfs.sh
+++ install_beegfs.sh
@@ -1,5 +1,5 @@
-yum install beegfs-mgmtd                               # management service
-yum install beegfs-meta libbeegfs-ib                   # metadata service; libbeegfs-ib is only required for RDMA
-yum install beegfs-storage libbeegfs-ib                # storage service; libbeegfs-ib is only required for RDMA
-yum install beegfs-client beegfs-helperd beegfs-utils  # client and command-line utils
-yum install beegfs-admon                               # admon service (optional)
+yum -y install beegfs-mgmtd                               # management service
+yum -y install beegfs-meta libbeegfs-ib                   # metadata service; libbeegfs-ib is only required for RDMA
+yum -y install beegfs-storage libbeegfs-ib                # storage service; libbeegfs-ib is only required for RDMA
+yum -y install beegfs-client beegfs-helperd beegfs-utils  # client and command-line utils
+yum -y install beegfs-admon                               # admon service (optional)
--- pre-install.sh
+++ pre-install.sh
@@ -1,1 +1,2 @@
 sed -i --follow-symlinks 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/sysconfig/selinux
+sudo wget -O /etc/yum.repos.d/beegfs-rhel7.repo https://www.beegfs.io/release/latest-stable/dists/beegfs-rhel7.repo
