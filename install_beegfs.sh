035c11d9c734aa12b05693834f904c3029c919d7
jmarquez <jmarquez@node1.jmarquez-qv74453.hpccloud-pg0.wisc.cloudlab.us>
2020-06-22 18:58:01 -0500

    adding privileges

--- install_beegfs.sh
+++ install_beegfs.sh
@@ -1,5 +1,5 @@
-yum -y install beegfs-mgmtd                               # management service
-yum -y install beegfs-meta libbeegfs-ib                   # metadata service; libbeegfs-ib is only required for RDMA
-yum -y install beegfs-storage libbeegfs-ib                # storage service; libbeegfs-ib is only required for RDMA
-yum -y install beegfs-client beegfs-helperd beegfs-utils  # client and command-line utils
-yum -y install beegfs-admon                               # admon service (optional)
+sudo yum -y install beegfs-mgmtd                               # management service
+sudo yum -y install beegfs-meta libbeegfs-ib                   # metadata service; libbeegfs-ib is only required for RDMA
+sudo yum -y install beegfs-storage libbeegfs-ib                # storage service; libbeegfs-ib is only required for RDMA
+sudo yum -y install beegfs-client beegfs-helperd beegfs-utils  # client and command-line utils
+sudo yum -y install beegfs-admon                               # admon service (optional)
